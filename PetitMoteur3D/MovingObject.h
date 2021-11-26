1d0a340af8500fba752bff5c24cf73cfb1585692
Luciano Porchet <porl2001@usherbrooke.ca>
2021-11-26 13:10:27 -0500

    commentaires pour MovingObject

--- PetitMoteur3D/MovingObject.h
+++ PetitMoteur3D/MovingObject.h
@@ -8,10 +8,13 @@
 class PM3D::IChargeur;
 class PM3D::CDispositifD3D11;
 
+//classe pour tous les objets qui bougent avec de la physique
 class MovingObject : public PM3D::CObjetMesh {
 
 public:
 	MovingObject(const std::string& nomfichier, PM3D::CDispositifD3D11* _pDispositif, float scale = 1);
+
+	//update la position et rotation de l'objet en fonction des calculs de physX
 	virtual void Anime(float tempEcoule) override;
 
 protected:
