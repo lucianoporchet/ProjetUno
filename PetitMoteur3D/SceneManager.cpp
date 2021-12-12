c1a90d31697870cb785a9865b735e11e9d71cff2
Luciano Porchet <lucianoporchet@gmail.com>
2021-12-12 22:36:10 +0100

    organisation textures skybox

--- PetitMoteur3D/SceneManager.cpp
+++ PetitMoteur3D/SceneManager.cpp
@@ -41,12 +41,13 @@ void SceneManager::InitObjects(PM3D::CDispositifD3D11* pDispositif, PM3D::CGesti
 		//Creation de la fausse skyBox (cube avec le culling inversé)
 		std::unique_ptr<PM3D::CBlocEffet1> skybox = std::make_unique<PM3D::CBlocEffet1>(BOXSIZE, BOXSIZE, BOXSIZE, pDispositif, i);
 		//ajoute une texture a la skybox
-		skybox->SetTextures(TexturesManager.GetNewTexture(L".\\modeles\\SkyBoxes\\up2.dds", pDispositif),
-							TexturesManager.GetNewTexture(L".\\modeles\\SkyBoxes\\down2.dds", pDispositif),
-							TexturesManager.GetNewTexture(L".\\modeles\\SkyBoxes\\left.dds", pDispositif),
-							TexturesManager.GetNewTexture(L".\\modeles\\SkyBoxes\\right2.dds", pDispositif),
-							TexturesManager.GetNewTexture(L".\\modeles\\SkyBoxes\\back.dds", pDispositif),
-							TexturesManager.GetNewTexture(L".\\modeles\\SkyBoxes\\front2.dds", pDispositif));
+		std::wstring texture = L".\\modeles\\SkyBoxes\\" + std::to_wstring(i);
+		skybox->SetTextures(TexturesManager.GetNewTexture(texture + L"\\up.dds"s, pDispositif),
+							TexturesManager.GetNewTexture(texture + L"\\down.dds", pDispositif),
+							TexturesManager.GetNewTexture(texture + L"\\left.dds", pDispositif),
+							TexturesManager.GetNewTexture(texture + L"\\right.dds", pDispositif),
+							TexturesManager.GetNewTexture(texture + L"\\back.dds", pDispositif),
+							TexturesManager.GetNewTexture(texture + L"\\front.dds", pDispositif));
 
 		Scenes[i].push_back(std::move(skybox));
 	}
